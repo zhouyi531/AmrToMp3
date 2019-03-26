a4fe5395bcf3cde46a3c8947d63755d242619a7e
Austin Zhou <yi.z@me.com>
2019-03-26 20:52:57 +0800

    no default export

--- src/AmrToMp3.ts
+++ src/AmrToMp3.ts
@@ -1,20 +1,18 @@
-import { exec } from "child_process";
-export * from "child_process";
-import * as path from "path";
-import ffmpeg from "ffmpeg-static";
+export * from 'child_process';
+import { exec } from 'child_process';
+import ffmpeg from 'ffmpeg-static';
+import * as path from 'path';
 
-export default class AmrToMp3 {
-  static async Convert(filePath: string, outputDir: string): Promise<string> {
+export class AmrToMp3 {
+  public static async Convert(filePath: string, outputDir: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
       const baseName = path.basename(filePath);
-      const etc = baseName.split(".").pop() as string;
-      const fileName = baseName.replace(`.${etc}`, "");
-      if (etc.toLowerCase() != "amr") {
-        reject("input must be amr file");
+      const etc = baseName.split('.').pop() as string;
+      const fileName = baseName.replace(`.${etc}`, '');
+      if (etc.toLowerCase() != 'amr') {
+        reject('input must be amr file');
       }
-      const cmdStr = `${
-        ffmpeg.path
-      } -y -i ${filePath} ${outputDir}/${fileName}.mp3`;
+      const cmdStr = `${ffmpeg.path} -y -i ${filePath} ${outputDir}/${fileName}.mp3`;
       exec(cmdStr, (err, stdout, stderr) => {
         if (err) {
           reject(`error: ${stderr}`);
--- src/__tests__/lib.test.ts
+++ src/__tests__/lib.test.ts
@@ -1,10 +1,10 @@
-import AmrToMp3 from "../AmrToMp3";
-import { expect } from "chai";
+import { expect } from 'chai';
+import { AmrToMp3 } from '../AmrToMp3';
 
-describe("general tests", () => {
-  it("Convert amr to mp3", async () => {
+describe('general tests', () => {
+  it('Convert amr to mp3', async () => {
     try {
-      const result = await AmrToMp3.Convert("./src/test.amr", "./");
+      const result = await AmrToMp3.Convert('./src/test.amr', './');
       expect(!!/mp3/.exec(result)).to.true;
       console.log(result);
     } catch (err) {
