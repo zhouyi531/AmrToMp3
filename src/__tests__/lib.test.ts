19adbd4982dcefa37213f2500c23188e4b08c7e0
Austin Zhou <yi.z@me.com>
2019-03-26 16:54:45 +0800

    conver amr to mp3

---
+++ src/__tests__/lib.test.ts
@@ -0,0 +1,14 @@
+import AmrToMp3 from "../AmrToMp3";
+import { expect } from "chai";
+
+describe("general tests", () => {
+  it("Convert amr to mp3", async () => {
+    try {
+      const result = await AmrToMp3.Convert("./src/test.amr", "./");
+      expect(!!/mp3/.exec(result)).to.true;
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+});
